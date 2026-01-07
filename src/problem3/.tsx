6819a794c6dd67b741a0235ef42a1b478a6107a5
Catalyst <dewkhaiyek@gmail.com>
2026-01-07 18:17:16 +0800

    Removed unwanted file and Added problem3 comments

--- src/problem3/.tsx
+++ src/problem3/.tsx
@@ -1,3 +1,30 @@
+/**
+  1. Interfaces
+  - added blockchain: string to WalletBalance
+  - FormattedWalletBalance extended from WalletBalance instead
+
+  2. WalletPage()
+  - added export
+  - React.FC always gives children prop which might not be good
+
+  3. getPriority()
+  - use map instead for scalability
+  - updated blockchain type to string
+
+  4. sortedBalances
+  - updated filter and sort structure for better readability and better handling for equal values 
+  - updated comparison <= 0 to >= 0
+
+  5. const formattedBalances
+  - added type
+  - toFixed(6), which is mostly for display only
+
+  6. const rows
+  - use formattedBalances instead
+  - used balance.amount in usdValue calculation since formattedAmount is a string and mostly used for display only
+  - added unique key
+ */
+
 interface WalletBalance {
   currency: string
   amount: number
